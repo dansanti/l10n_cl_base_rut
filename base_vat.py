f0a5828cb4dbd4444ee02db509082991251b4f60
dansanti <dansanti@gmail.com>
2016-09-17 10:20:21 -0300

    Se elimina código no necesario

--- base_vat.py
+++ base_vat.py
@@ -32,15 +32,13 @@ class res_partner(models.Model):
 
     @api.onchange('formated_vat')
     def onchange_document(self):
-        mod_obj = self.env['ir.model.data']
         formated_vat = (
             re.sub('[^1234567890Kk]', '',
             str(self.formated_vat))).zfill(9).upper()
 
         self.vat = 'CL%s' % formated_vat
 
-        
+
         self.formated_vat = '%s.%s.%s-%s' % (
             formated_vat[0:2], formated_vat[2:5], formated_vat[5:8],
             formated_vat[-1])
-
