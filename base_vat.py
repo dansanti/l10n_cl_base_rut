e42efa2f616f3f65e49afe6bbef5ea1a9d12f9bb
dansanti <dansanti@gmail.com>
2016-09-17 10:11:22 -0300

    Revert Commit

--- base_vat.py
+++ base_vat.py
@@ -7,8 +7,28 @@ class res_partner(models.Model):
     _inherit = 'res.partner'
 
     formated_vat = fields.Char(
-        translate=True, string='Printable VAT',
-        store=True, help='Show formatted vat')
+        translate=True, string='Printable VAT', store=True,
+        help='Show formatted vat')
+
+    def check_vat_cl (self, vat):
+        body, vdig = '', ''
+        if len(vat) > 9:
+            vat = vat.replace('-','',1).replace('.','',2)
+        if len(vat) != 9:
+            return False
+        else:
+            body, vdig = vat[:-1], vat[-1].upper()
+        try:
+            vali = range(2,8) + [2,3]
+            operar = '0123456789K0'[11 - (
+                sum([int(digit)*factor for digit, factor in zip(
+                    body[::-1],vali)]) % 11)]
+            if operar == vdig:
+                return True
+            else:
+                return False
+        except IndexError:
+            return False
 
     @api.onchange('formated_vat')
     def onchange_document(self):
@@ -18,5 +38,9 @@ class res_partner(models.Model):
             str(self.formated_vat))).zfill(9).upper()
 
         self.vat = 'CL%s' % formated_vat
+
         
-        self.formated_vat = formated_vat[0:2] + '.' + formated_vat[2:5] + '.' + formated_vat[5:8] +'-' + formated_vat[-1]
+        self.formated_vat = '%s.%s.%s-%s' % (
+            formated_vat[0:2], formated_vat[2:5], formated_vat[5:8],
+            formated_vat[-1])
+
