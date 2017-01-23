29b366970de36b6ce68eefc490ffa13647078653
dansanti <dansanti@mail.com>
2017-01-23 03:18:02 -0300

    Se elimina Código y campos no necesarios

--- base_vat.py
+++ base_vat.py
@@ -2,18 +2,11 @@
 from openerp import models, fields, api
 import re
 
-
 class res_partner(models.Model):
     _inherit = 'res.partner'
 
-    formated_vat = fields.Char(
-        translate=True, string='Printable VAT', store=True,
-        help='Show formatted vat')
-
     def check_vat_cl (self, vat):
         body, vdig = '', ''
-        if len(vat) > 9:
-            vat = vat.replace('-','',1).replace('.','',2)
         if len(vat) != 9:
             return False
         else:
@@ -29,16 +22,3 @@ class res_partner(models.Model):
                 return False
         except IndexError:
             return False
-
-    @api.onchange('formated_vat')
-    def onchange_document(self):
-        formated_vat = (
-            re.sub('[^1234567890Kk]', '',
-            str(self.formated_vat))).zfill(9).upper()
-
-        self.vat = 'CL%s' % formated_vat
-
-
-        self.formated_vat = '%s.%s.%s-%s' % (
-            formated_vat[0:2], formated_vat[2:5], formated_vat[5:8],
-            formated_vat[-1])
