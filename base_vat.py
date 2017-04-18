f1d0cac754f911f2cc466520d41ed031c81e711d
dansanti <dansanti@mail.com>
2017-04-18 14:04:48 -0300

    [IMP]Constrain para No permitir duplicar rut

--- base_vat.py
+++ base_vat.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
-from openerp import models, fields, api
+from openerp import models, fields, api, _
 import re
+from openerp.exceptions import UserError
+import logging
+_logger = logging.getLogger(__name__)
 
 class res_partner(models.Model):
     _inherit = 'res.partner'
 
+    @api.constrains('vat')
+    def _rut_unique(self):
+        partner = self.env['res.partner'].search([('vat','=',self.vat),('id','!=', self.id)])
+        if self.vat !="CL555555555" and partner:
+            raise UserError(_('El rut debe ser único'))
+            return False
+
     def check_vat_cl(self, vat):
         body, vdig = '', ''
-        if vat == '555555555':
-            return True
         if len(vat) != 9:
             return False
         else:
